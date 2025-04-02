83fdfbbabf0f4fc83a99330c9d057f5e4313d798
matheusfefagundes <matheusferreirafagundes23@gmail.com>
2025-04-02 09:19:34 -0300

    página de login e cadastro feita

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Servir arquivos estáticos da pasta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static("public"));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
